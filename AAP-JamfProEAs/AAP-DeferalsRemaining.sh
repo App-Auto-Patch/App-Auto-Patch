213ea519c13a2199f0b2b3074cacb3d66654fdb4
Andrew Spokes <132794591+TechTrekkie@users.noreply.github.com>
2024-03-20 19:01:56 -0700

    Update AAP-DeferalsRemaining.sh

--- AAP-JamfProEAs/AAP-DeferalsRemaining.sh
+++ AAP-JamfProEAs/AAP-DeferalsRemaining.sh
@@ -2,13 +2,13 @@
 
 # This script returns the deferrals remaining of App Auto Patch to Jamf inventory. 
 # Make sure to set the Extension Attribute Data Type to "String".
-# 12.19.2023 @andrewbarnett
+# 03.20.24 @techtrekkie - updated to read from AppAutoPatchStatus.plist
 
 # Path to the App Auto Patch working folder:
 AAP_folder="/Library/Application Support/AppAutoPatch"
 
 # Path to the local property list file:
-AAP_plist="${AAP_folder}/AppAutoPatchDeferrals.plist"
+AAP_plist="${AAP_folder}/AppAutoPatchStatus.plist"
 
 # Report if the App Auto Patch preference file exists.
 if [[ -f "${AAP_plist}" ]]; then
