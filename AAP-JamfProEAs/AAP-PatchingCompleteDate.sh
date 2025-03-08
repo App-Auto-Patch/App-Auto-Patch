98d83654cf38a491f01fb54786582c8718245fb2
Andrew Spokes <132794591+TechTrekkie@users.noreply.github.com>
2025-03-07 22:08:03 -0700

    3.0 EA Updates

--- AAP-JamfProEAs/AAP-PatchingCompleteDate.sh
+++ AAP-JamfProEAs/AAP-PatchingCompleteDate.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# This script returns the Patching Complete Date of App Auto Patch to Jamf inventory. 
+# This script returns the Patching Complete Date of App Auto Patch 3.0.0 to Jamf inventory. 
 # Make sure to set the Extension Attribute Data Type to "Date".
-# https://techitout.xyz/app-auto-patch
 # by Andrew Spokes (@techtrekkie)
-# 03.25.2024
+# 03.07.2025
 
 # Path to the App Auto Patch working folder:
-AAP_folder="/Library/Application Support/AppAutoPatch"
+AAP_folder="/Library/Management/AppAutoPatch"
 
 # Path to the local property list file:
-AAP_plist="${AAP_folder}/AppAutoPatchStatus" # No trailing ".plist"
+AAP_plist="${AAP_folder}/xyz.techitout.appAutoPatch" # No trailing ".plist"
 
 # Report if the App Auto Patch preference file exists.
 if [[ -f "${AAP_plist}.plist" ]]; then
